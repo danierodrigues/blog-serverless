10044e631ffa8ed27aba24714614d6ead3228faf
Daniel Rodrigues <daniel.rodrigues.cavadas@gmail.com>
2021-08-31 07:48:02 +0100

    Added authors serverless

--- serverless-authors/controllers/author.js
+++ serverless-authors/controllers/author.js
@@ -4,9 +4,23 @@ const routes = express.Router({
 });
 
 
-routes.get('/', (req, res) => {
-    res.status(200).json({"teste":"Correto"});
+routes.post('/', (req, res) => {
+    let newname = req.body.newname; 
+    let array = req.body.array;
+
+    if(!array){
+        array = [newname];
+        return res.status(200).json({"ok":true, "arr": array});
+    }
+
+    let isNotNewName = array.filter(name => name === newname);
+
+    if(isNotNewName.length !== 0) return res.status(400).json({"ok":false});
+
+    array.push(newname);
+    return res.status(200).json({"ok":true, "arr": array});
 });
+
 
 module.exports = {
     routes
--- serverless-authors/index.js
+++ serverless-authors/index.js
@@ -6,6 +6,8 @@ const {routes: authorRoutes} = require('./controllers/author');
 const app = express();
 
 app.use(cors());
-app.use('/author', userRoutes);
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use('/author',authorRoutes);
 
 module.exports = app;
